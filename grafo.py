86b09f20f786b550784f062ef2cb9a5b1543c548
lain <arthur.zachow@gmail.com>
2016-12-08 18:33:23 -0200

    Kosaraju algorithm

--- grafo.py
+++ grafo.py
@@ -25,6 +25,70 @@ class SimpleGraph:
     def neighbors(self, id):
         return self.edges[id]
 
+import sys
+import time
+import resource
+from itertools import groupby
+from collections import defaultdict
+ 
+ 
+#set rescursion limit and stack size limit
+sys.setrecursionlimit(10 ** 6)
+resource.setrlimit(resource.RLIMIT_STACK, (2 ** 29, 2 ** 30))
+
+class Track(object):
+    """Keeps track of the current time, current source, component leader,
+    finish time of each node and the explored nodes."""
+
+    def __init__(self):
+        self.current_time = 0
+        self.current_source = None
+        self.leader = {}
+        self.finish_time = {}
+        self.explored = set()
+
+def dfs(graph_dict, node, track):
+    """Inner loop explores all nodes in a SCC. Graph represented as a dict,
+    {tail node: [head nodes]}. Depth first search runs recrusively and keeps
+    track of the parameters"""
+
+    track.explored.add(node)
+    track.leader[node] = track.current_source
+    for head in graph_dict[node]:
+        if head not in track.explored:
+            dfs(graph_dict, head, track)
+    track.current_time += 1
+    track.finish_time[node] = track.current_time
+
+
+def dfs_loop(graph_dict, nodes, track):
+    """Outter loop checks out all SCCs. Current source node changes when one
+    SCC inner loop finishes."""
+
+    for node in nodes:
+        if node not in track.explored:
+            track.current_source = node
+            dfs(graph_dict, node, track)
+
+
+def scc(graph, reverse_graph, nodes):
+    """First runs dfs_loop on reversed graph with nodes in decreasing order,
+    then runs dfs_loop on orignial graph with nodes in decreasing finish
+    time order(obatined from firt run). Return a dict of {leader: SCC}."""
+
+    out = defaultdict(list)
+    track = Track()
+    dfs_loop(reverse_graph, nodes, track)
+    sorted_nodes = sorted(track.finish_time,
+                          key=track.finish_time.get, reverse=True)
+    track.current_time = 0
+    track.current_source = None
+    track.explored = set()
+    dfs_loop(graph, sorted_nodes, track)
+    for lead, vertex in groupby(sorted(track.leader, key=track.leader.get),
+                                key=track.leader.get):
+        out[lead] = list(vertex)
+    return out
 
 def read_file(file_path):
     graph = SimpleGraph()
@@ -40,11 +104,10 @@ def read_file(file_path):
         if line:
             for node in line:
                 try:
-                    if graph.edges[line[0]]:
-                        graph.edges[line[0]].append(node)
+                    graph.edges[int(line[0])].append(int(node))
                 except KeyError:
                     if node is not line[0]:
-                        graph.edges.update({line[0]:[node]})
+                        graph.edges.update({int(line[0]):[int(node)]})
 
     return graph
 
@@ -92,13 +155,24 @@ if __name__ == '__main__':
     ARGS = PRS.parse_args()
     FILENAME = ARGS.file
     GRAPH = read_file(FILENAME)
+    rev = {
+        0: [],
+        1: [0, 3],
+        2: [5],
+        3: [4],
+        4: [1],
+        5: [2, 4]
+    }
+
     comp = []
     print('Graph:')
     print(GRAPH.edges)
-
+    print(scc(GRAPH.edges, rev, [0, 1, 2, 3, 4, 5]))
+"""
     for key in GRAPH.edges:
         print(key + ':')
         visited = breadth_search(GRAPH, key)
         comp.append(list(visited.keys()))
         print()
     print(comp)
+"""
